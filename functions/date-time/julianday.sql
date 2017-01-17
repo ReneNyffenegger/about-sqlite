b3e8ac042e121883dac4e939d644292003eb3aed
René Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2017-01-17 14:16:53 +0100

    Date arithmetic

--- functions/date-time/julianday.sql
+++ functions/date-time/julianday.sql
@@ -2,3 +2,10 @@
 select date('now') || ' -> ' || julianday('now', 'start of day');
 --
 -- 2017-01-17 -> 2457770.5
+
+-- Date arithmetic:
+select julianday('2017-08-28') - julianday('2017-07-28');
+-- 31.0
+
+select julianday('2017-08-28 06:00:00') - julianday('2017-07-28');
+-- 31.25
