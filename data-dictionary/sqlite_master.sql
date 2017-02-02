ad1f7b510ac8334259483f1d653020de8ba98163
René Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2017-02-02 13:13:57 +0100

    + sqlite_master.sql

---
+++ data-dictionary/sqlite_master.sql
@@ -0,0 +1,49 @@
+create table foo (
+  id,
+  col_1,
+  col_2  text,
+  col_3  check (col_3 > 5 and col_3 < 10),
+  --
+  constraint foo_pk primary key (id),
+  constraint foo_uq unique(col_2, col_3)
+);
+
+create index foo_ix on foo (col_2);
+
+create view foo_v as
+select
+  col_1,
+  col_3,
+  col_1 + col_2 sum_col
+from
+  foo;
+
+
+select type, name from sqlite_master where tbl_name = 'foo';
+--
+-- table|foo
+-- index|sqlite_autoindex_foo_1
+-- index|sqlite_autoindex_foo_2
+-- index|foo_ix
+
+
+select type, name from sqlite_master where tbl_name = 'foo_v';
+--
+-- view|foo_v
+
+
+select sql from sqlite_master where name = 'foo';
+--
+-- CREATE TABLE foo (
+--   id,
+--   col_1,
+--   col_2  text,
+--   col_3  check (col_3 > 5 and col_3 < 10),
+--   --
+--   constraint foo_pk primary key (id),
+--   constraint foo_uq unique(col_2, col_3)
+-- )
+
+  
+
+drop table foo;
