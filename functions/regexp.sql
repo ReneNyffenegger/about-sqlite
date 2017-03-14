31d010fd7c3383961227eaaf3779120d53d74735
René Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2017-03-14 09:59:03 +0100

    + regexp.sql

---
+++ functions/regexp.sql
@@ -0,0 +1,22 @@
+create table tq84_regexp (
+  col text
+);
+
+insert into tq84_regexp values ('foo');
+insert into tq84_regexp values ('twenty-two: 42');
+
+-- pcre.so: needed for regexp (Get it on Ubuntu with
+--   sudo apt-get install sqlite3-pcre
+-- )
+--
+.load /usr/lib/sqlite3/pcre.so
+
+select col from tq84_regexp where col regexp '\d';
+--
+-- twenty-two: 42
+
+select col from tq84_regexp where col regexp '(.)\1';
+--
+-- foo
+
+drop table tq84_regexp;
