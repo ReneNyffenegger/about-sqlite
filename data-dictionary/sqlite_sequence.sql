eba722f37e7c3290820c428d8a16742741a85ad5
René Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2017-03-22 11:58:15 +0100

    + sqlite_sequence.sql

---
+++ data-dictionary/sqlite_sequence.sql
@@ -0,0 +1,32 @@
+create table tq84_tab (
+  id   integer primary key autoincrement,
+  col
+);
+
+.schema sqlite_sequence
+--
+-- CREATE TABLE sqlite_sequence(name,seq);
+
+insert into tq84_tab (col) values ('foo');
+insert into tq84_tab (col) values ('bar');
+
+select * from sqlite_sequence where name = 'tq84_tab';
+--
+-- tq84_tab|2
+
+insert into tq84_tab values(5, 'five');
+
+select * from sqlite_sequence where name = 'tq84_tab';
+--
+-- tq84_tab|5
+
+insert into tq84_tab (col) values ('baz');
+select id from tq84_tab where col = 'baz';
+--
+-- 6
+
+select * from sqlite_sequence where name = 'tq84_tab';
+--
+-- tq84_tab|6
+
+drop table tq84_tab;
