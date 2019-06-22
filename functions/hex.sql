db210ed2d183cc5cb7e7505cd9f391198def7cec
René Nyffenegger <mail@renenyffenegger.ch>
2019-06-22 07:37:28 +0200

    blob and numericals

--- functions/hex.sql
+++ functions/hex.sql
@@ -2,16 +2,27 @@ create table h (
   col
 );
 
-insert into h values ('a');
-insert into h values ('ä');
-insert into h values ('foo');
+insert into h values ( 'a'       );
+insert into h values ( 'ä'       );
+insert into h values ( 'foo'     );
+insert into h values (x'deadbeef');
+insert into h values (  null     );
+insert into h values (  42       );
+insert into h values (  9.9      );
 
 .mode column
-.width 3 6
+.width 14 10
 
-select col, hex(col) from h;
--- 
--- a    61    
--- ä   C3A4          <-- c3 a4 is utf8 for ä
--- foo  666F6F
+select
+   quote(col),
+   hex(col)
+from
+   h;
 --
+--  'a'             61        
+--  'ä'             C3A4          <-- c3 a4 is utf8 for ä
+--  'foo'           666F6F    
+--  X'DEADBEEF'     DEADBEEF  
+--  NULL                      
+--  42              3432      
+--  9.9             392E39
