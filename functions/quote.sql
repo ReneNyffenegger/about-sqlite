4dd89ebff5e01de73a7784b71c80350e3cbc52f1
René Nyffenegger <mail@renenyffenegger.ch>
2019-06-22 06:46:57 +0200

    + blob

--- functions/quote.sql
+++ functions/quote.sql
@@ -1,8 +1,9 @@
 create table tq84_quote(t);
 
-insert into tq84_quote values ('foo\bar');
-insert into tq84_quote values ("foo\nar");
-insert into tq84_quote values ('Bill O''Dill');
+insert into tq84_quote values ( 'foo\bar'     );
+insert into tq84_quote values ( "foo\nar"     );
+insert into tq84_quote values ( 'Bill O''Dill');
+insert into tq84_quote values (x'deadbeef'    );
 
 .mode column
 .width 15 15
@@ -11,6 +12,7 @@ select t, quote(t) from tq84_quote;
 -- foo\bar          'foo\bar'      
 -- foo\nar          'foo\nar'      
 -- Bill O'Dill      'Bill O''Dill' 
+-- ��               X'DEADBEEF'
 
 
 drop table tq84_quote;
