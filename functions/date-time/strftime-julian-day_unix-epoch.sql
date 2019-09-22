df6fa9c1b98b48099a1d2fe0b747943a6b318e91
René Nyffenegger <mail@renenyffenegger.ch>
2019-09-22 19:49:06 +0200

    add julianday()

--- functions/date-time/strftime-julian-day_unix-epoch.sql
+++ functions/date-time/strftime-julian-day_unix-epoch.sql
@@ -2,10 +2,12 @@
 .header on
 
 select
-          strftime('%J', '1970-01-01 00:00:00')  JulianDay,
-          strftime('%s', '1970-01-01 00:00:00')  UnixEpoch,
-   typeof(strftime('%J', '1970-01-01 00:00:00')) dataType;
+          strftime ('%J', '1970-01-01 00:00:00')  julDay_J  ,
+          julianday(      '1970-01-01 00:00:00')  julDay    ,
+          strftime ('%s', '1970-01-01 00:00:00')  unixEpoch ,
+   typeof(strftime ('%J', '1970-01-01 00:00:00')) dataType_1,
+   typeof(julianday(      '1970-01-01 00:00:00')) dataType_2
 --
--- JulianDay   UnixEpoch   dataType  
--- ----------  ----------  ----------
--- 2440587.5   0           text      
+-- julDay_J    julDay      unixEpoch   dataType_1  dataType_2
+-- ----------  ----------  ----------  ----------  ----------
+-- 2440587.5   2440587.5   0           text        real      
