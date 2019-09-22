dfc1b2bcf5e01e5b979d6aaadd5c8b06a44eaeb2
René Nyffenegger <mail@renenyffenegger.ch>
2019-09-22 18:52:08 +0200

    start of julian day

--- functions/date-time/julianday.sql
+++ functions/date-time/julianday.sql
@@ -2,10 +2,11 @@
 .header on
 
 select
+    datetime ('0'                  )                           start_of_julian_day,
     julianday('now', 'start of day')                           todays_julian_day,
     julianday('2017-08-28'         ) - julianday('2017-07-28') days_between,
     julianday('2017-08-28 06:00:00') - julianday('2017-07-28') fractional_days_between;
 --
--- todays_julian_day  days_between  fractional_days_between
--- -----------------  ------------  -----------------------
--- 2458748.5          31.0          31.25                  
+-- start_of_julian_day   todays_julian_day  days_between  fractional_days_between
+-- --------------------  -----------------  ------------  -----------------------
+-- -4713-11-24 12:00:00  2458748.5          31.0          31.25                  
