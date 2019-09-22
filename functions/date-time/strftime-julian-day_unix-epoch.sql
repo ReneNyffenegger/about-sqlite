91891435193a094472b6fed22645f0b05f45fa81
René Nyffenegger <mail@renenyffenegger.ch>
2019-09-22 19:45:26 +0200

    add result

--- functions/date-time/strftime-julian-day_unix-epoch.sql
+++ functions/date-time/strftime-julian-day_unix-epoch.sql
@@ -4,5 +4,8 @@
 select
           strftime('%J', '1970-01-01 00:00:00')  JulianDay,
           strftime('%s', '1970-01-01 00:00:00')  UnixEpoch,
-   typeof(strftime('%J', '1970-01-01 00:00:00')) dataType
-;
+   typeof(strftime('%J', '1970-01-01 00:00:00')) dataType;
+--
+-- JulianDay   UnixEpoch   dataType  
+-- ----------  ----------  ----------
+-- 2440587.5   0           text      
