6cdd42e55a14468eb161a513988132d6454f0c0e
René Nyffenegger <mail@renenyffenegger.ch>
2019-09-22 17:37:26 +0200

    + now.sql

---
+++ functions/date-time/now.sql
@@ -0,0 +1,11 @@
+.mode   column
+.header on
+select
+   time    (                             'now') "time",
+   date    (                             'now') "date",
+   datetime(                             'now') "datetime",
+   strftime('Minutes past the hour: %M', 'now') "strftime";
+--
+-- time        date        datetime             strftime                 
+-- ----------  ----------  -------------------  -------------------------
+-- 15:32:24    2019-09-22  2019-09-22 15:32:24  Minutes past the hour: 32
