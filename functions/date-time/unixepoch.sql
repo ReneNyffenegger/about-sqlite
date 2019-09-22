311dc6e84bfed0b6199b7afc8fd17cb2e160d170
René Nyffenegger <mail@renenyffenegger.ch>
2019-09-22 18:36:23 +0200

    + unixepoch.sql

---
+++ functions/date-time/unixepoch.sql
@@ -0,0 +1,25 @@
+.mode column
+.header    on
+
+select
+   datetime(0, 'unixepoch') start_of_unix_epoch,
+   datetime( (2019 - 1970) * 365 * 24 * 60 * 60 +
+                              12 * 24 * 60 * 60 +  -- 12 Leap years
+                               7 * 24 * 60 * 60 +  --  8th day of the year (epoch starts with first!)
+                                   15 * 60 * 60 +  --  15th hour
+                                        32 * 60 +  --  32th minute
+                                              7 ,  --  7 second
+                             'unixepoch')  calculated;
+;
+
+
+
+
+
+
+
+
+
+
+
+
