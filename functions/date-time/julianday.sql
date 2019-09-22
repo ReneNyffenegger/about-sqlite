429d8d324bc0ebe9fe4cb22c5920bde81e9dc338
René Nyffenegger <mail@renenyffenegger.ch>
2019-09-22 18:47:43 +0200

    improve

--- functions/date-time/julianday.sql
+++ functions/date-time/julianday.sql
@@ -1,11 +1,11 @@
+.mode   column
+.header on
 
-select date('now') || ' -> ' || julianday('now', 'start of day');
+select
+    julianday('now', 'start of day')                           todays_julian_day,
+    julianday('2017-08-28'         ) - julianday('2017-07-28') days_between,
+    julianday('2017-08-28 06:00:00') - julianday('2017-07-28') fractional_days_between;
 --
--- 2017-01-17 -> 2457770.5
-
--- Date arithmetic:
-select julianday('2017-08-28') - julianday('2017-07-28');
--- 31.0
-
-select julianday('2017-08-28 06:00:00') - julianday('2017-07-28');
--- 31.25
+-- todays_julian_day  days_between  fractional_days_between
+-- -----------------  ------------  -----------------------
+-- 2458748.5          31.0          31.25                  
