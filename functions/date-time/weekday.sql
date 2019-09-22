d3756d9b16d8bc5c02c9d0337e12135212268e3b
René Nyffenegger <mail@renenyffenegger.ch>
2019-09-22 19:44:48 +0200

    add result

--- functions/date-time/weekday.sql
+++ functions/date-time/weekday.sql
@@ -9,3 +9,7 @@ select
    date('2019-09-20', 'weekday 4') next_thursday,
    date('2019-09-20', 'weekday 5') next_friday,
    date('2019-09-20', 'weekday 6') next_saturday;
+--
+-- next_sunday  next_monday  next_tuesday  next_wednesday  next_thursday  next_friday  next_saturday
+-- -----------  -----------  ------------  --------------  -------------  -----------  -------------
+-- 2019-09-22   2019-09-23   2019-09-24    2019-09-25      2019-09-26     2019-09-20   2019-09-21   
