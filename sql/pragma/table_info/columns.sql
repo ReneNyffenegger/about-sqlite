7128038e627487c4c617fecc97b88fa4b4979a58
René Nyffenegger <mail@renenyffenegger.ch>
2020-04-02 12:51:15 +0200

    + columns.sql

---
+++ sql/pragma/table_info/columns.sql
@@ -0,0 +1,14 @@
+.header on
+.mode   columns
+.width  3 4 4 7 10 2
+
+pragma table_info('pragma_table_info');
+--
+-- cid  name  type  notnull  dflt_value  pk
+-- ---  ----  ----  -------  ----------  --
+-- 0    cid         0                    0 
+-- 1    name        0                    0 
+-- 2    type        0                    0 
+-- 3    notn        0                    0 
+-- 4    dflt        0                    0 
+-- 5    pk          0                    0 
