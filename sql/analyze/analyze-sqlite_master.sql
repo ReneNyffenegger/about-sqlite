8cd239d44089d05cb18545992c17bde4b6f77960
René Nyffenegger <mail@renenyffenegger.ch>
2020-04-02 10:36:23 +0200

    rm analyze-sqlite_master.sql

--- sql/analyze/analyze-sqlite_master.sql
+++
@@ -1,8 +0,0 @@
---
--- When an application changes the statistics tables directly,
--- SQLite will not immediately notice the changes.
---
--- An application can force the query planner to reread the
--- statistics tables by running
-
-analyze sqlite_master;
