0234583b2744118f278f2f48a4399b9a6e82f572
René Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2020-11-14 21:17:34 +0100

    improve statement

--- sql/update/update_with_aggregate_function.sql
+++ sql/update/update_with_aggregate_function.sql
@@ -7,65 +7,54 @@ drop table if exists tq84_child;
 drop table if exists tq84_parent;
 
 create table tq84_parent (
-  id                int primary key,
+  id                text primary key,
   avg_val           float,
   min_val           float,
   max_val           float
 );
 
 create table tq84_child (
-  id_parent         int references tq84_parent,
+  id_parent         text references tq84_parent,
   val               int
 );
 
 
-insert into tq84_parent (id) values(1);
-insert into tq84_parent (id) values(2);
+insert into tq84_parent (id) values('A');
+insert into tq84_parent (id) values('B');
 
-insert into tq84_child values (1,  1);
-insert into tq84_child values (1,  2);
-insert into tq84_child values (1,  3);
+insert into tq84_child values ('A',  1);
+insert into tq84_child values ('A',  2);
+insert into tq84_child values ('A',  3);
 
-insert into tq84_child values (2,  6);
-insert into tq84_child values (2,  7);
-insert into tq84_child values (2,  8);
-insert into tq84_child values (2,  9);
-insert into tq84_child values (2, 10);
+insert into tq84_child values ('B',  6);
+insert into tq84_child values ('B',  7);
+insert into tq84_child values ('B',  8);
+insert into tq84_child values ('B',  9);
+insert into tq84_child values ('B', 10);
 
---
--- Does not work
---
--- update
---   tq84_parent
--- set
---  (
---   avg_val,
---   min_val,
---   max_val
---  )
--- = (select
---      avg(val),
---      min(val),
---      max(val)
---    from
---      tq84_child
---    where
---      id_parent = id
--- );
-
-update 
+update
   tq84_parent
 set
-  avg_val = (select avg(val) from tq84_child where id_parent = id),
-  min_val = (select min(val) from tq84_child where id_parent = id),
-  max_val = (select max(val) from tq84_child where id_parent = id);
-
-
-
+ (
+  avg_val,
+  min_val,
+  max_val
+ )
+= (select
+     avg(val),
+     min(val),
+     max(val)
+   from
+     tq84_child
+   where
+     id_parent = id
+);
 
 .mode columns
 
 select * from tq84_parent;
-
-
- 
+--
+--  id  avg_val  min_val  max_val
+--  --  -------  -------  -------
+--  A   2.0      1.0      3.0
+--  B   8.0      6.0      10.0
